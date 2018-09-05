1eba143ee9c3f2905ec34ac512d8f87494ccd54f
Timothy Jones <tjones@dius.com.au>
2018-09-05 16:18:10 +1000

    Remove node http endpoints

--- example-config.sh
+++ example-config.sh
@@ -3,4 +3,3 @@
 export GITHUB_CLIENT_ID=<GitHub Client ID>
 export GITHUB_CLIENT_SECRET=<GitHub Client Secret>
 export COGNITO_REDIRECT_URI=https://<Your Cognito Domain>/oauth2/idpresponse
-export PORT=<Port to start the server on>
