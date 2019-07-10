9d857c3cb53aafcf875cc12db4374392ef969204
Sebastian Just <Sebastian_Just@comcast.com>
2019-07-10 11:37:56 -0400

    Fix broken key generation on OSX 10.14.5 (Mojave)
    On Mojave, `ssh-keygen` `OpenSSH_7.9p1, LibreSSL 2.7.3`
    does not generate the key in PEM format causing JWT to fail
    the signing with a `bad signature` error

--- scripts/create-key.sh
+++ scripts/create-key.sh
@@ -10,6 +10,6 @@ KEY_FILE="$PROJECT_ROOT"/jwtRS256.key
 
 if [ ! -f "$KEY_FILE" ]; then
   echo "  --- Creating private key, as it does not exist ---"
-  ssh-keygen -t rsa -b 4096 -f "$KEY_FILE" -N ''
+  ssh-keygen -t rsa -b 4096 -m PEM -f "$KEY_FILE" -N ''
   openssl rsa -in "$KEY_FILE" -pubout -outform PEM -out "$KEY_FILE".pub
 fi
