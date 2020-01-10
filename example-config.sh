cf9b591c5eb1ccd3b37a83a3ab042cc2970b4a80
Timothy Jones <tim.jones@versent.com.au>
2020-01-10 13:26:46 +1100

    fix(config): Add stage name variable to example config

--- example-config.sh
+++ example-config.sh
@@ -20,6 +20,7 @@ export COGNITO_REDIRECT_URI=# https://<Your Cognito Domain>/oauth2/idpresponse
 export BUCKET_NAME=# An S3 bucket name to use as the deployment pipeline
 export STACK_NAME=# The name of the stack to create
 export REGION=# AWS region to deploy the stack and bucket in
+export STAGE_NAME=# Stage name to create and deploy to in API gateway
 
 # Variables required if deploying a node http server
 export PORT=# <Port to start the server on>
