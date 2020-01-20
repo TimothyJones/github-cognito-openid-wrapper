2ca44d0adee754e3a1567b6cc72c798dc7f75da4
Danny Douglass <266481+DannyDouglass@users.noreply.github.com>
2020-01-20 15:36:34 -0800

    dynamic region

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  AWS_REGION: process.env.AWS_REGION,
+  COGNITO_REDIRECT_URI: process.env.COGNITO_REDIRECT_URI,
   DYNAMODB_STATE_TABLE: process.env.DYNAMODB_STATE_TABLE,
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-  COGNITO_REDIRECT_URI: process.env.COGNITO_REDIRECT_URI,
   GITHUB_API_URL: process.env.GITHUB_API_URL,
   GITHUB_LOGIN_URL: process.env.GITHUB_LOGIN_URL,
   PORT: parseInt(process.env.PORT, 10) || undefined,
