bb3ace5b87aa74200e226e72bfa21f59df90a8d0
Timothy Jones <timothy.l.jones@gmail.com>
2021-07-08 14:49:49 +1000

    fix: Always export API base URLs

--- example-config.sh
+++ example-config.sh
@@ -4,8 +4,11 @@
 export GITHUB_CLIENT_ID=# <GitHub OAuth App Client ID>
 export GITHUB_CLIENT_SECRET=# <GitHub OAuth App Client Secret>
 export COGNITO_REDIRECT_URI=# https://<Your Cognito Domain>/oauth2/idpresponse
+# Change these if used with GitHub Enterprise (see below)
+export GITHUB_API_URL=https://api.github.com
+export GITHUB_LOGIN_URL=https://github.com
 
-# Variables required if used with GitHub Enterprise
+# Alternate URLs if used with GitHub Enterprise
 # GITHUB_API_URL=# https://<GitHub Enterprise Host>/api/v3
 # GITHUB_LOGIN_URL=# https://<GitHub Enterprise Host>
 
